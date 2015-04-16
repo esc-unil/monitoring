b08d8dc1eb5d4a272863705355c313048067d69f
Thomas Pineau <tpineau.esc@gmail.com>
2015-04-16 10:57:05 +0200

    reformat code

--- bing.js
+++ bing.js
@@ -1,8 +1,8 @@
 'use strict';
 
 /*
-auteur: tpineau
-*/
+ auteur: tpineau
+ */
 
 var querystring = require('querystring');
 var request = require('request');
@@ -12,124 +12,144 @@ var keys = require('./keys'); //fichier keys.js contenant les clefs des APIs
 
 var bingKey = keys.bingKey;
 
-function webSearch(keyword, num, opt_args, callback){
+function webSearch(keyword, num, opt_args, callback) {
 // Fonction de recherche de page web via Bing
 // Informations sur les arguments optionnels (opt_args): https://datamarket.azure.com/dataset/bing/search#schema 
-	if (typeof opt_args === 'function'){callback = opt_args; opt_args = null;}
-	var args = {Query:'\'' + keyword + '\'', top: num, format:'json'};
-	extend(args, opt_args);
-	go('Web', args, function(err, response) {
-		if (err) callback(err);
-		var results = [];
-		for (var i= 0; i < response.length; i++){
-			var obj = {request:{}};
-			obj.url = response[i].Url;
-			obj.request.type = 'web';
-			obj.request.keyword = keyword;
-			obj.request.ranking = i+1;
-			obj.request.platform = 'Bing';
-			obj.request.date = new Date();
-			obj.request.args = opt_args;
-			results.push(obj);
-		}
-		callback(null, results);
-	});
+    if (typeof opt_args === 'function') {
+        callback = opt_args;
+        opt_args = null;
+    }
+    var args = {Query: '\'' + keyword + '\'', top: num, format: 'json'};
+    extend(args, opt_args);
+    go('Web', args, function (err, response) {
+        if (err) callback(err);
+        var results = [];
+        for (var i = 0; i < response.length; i++) {
+            var obj = {request: {}};
+            obj.url = response[i].Url;
+            obj.request.type = 'web';
+            obj.request.keyword = keyword;
+            obj.request.ranking = i + 1;
+            obj.request.platform = 'Bing';
+            obj.request.date = new Date();
+            obj.request.args = opt_args;
+            results.push(obj);
+        }
+        callback(null, results);
+    });
 }
 
-function imagesSearch(keyword, num, opt_args, callback){
+function imagesSearch(keyword, num, opt_args, callback) {
 // Fonction de recherche d'images via Google
 // Informations sur les arguments optionnels (opt_args): https://datamarket.azure.com/dataset/bing/search#schema
-	if (typeof opt_args === 'function'){callback = opt_args; opt_args = null;}
-	var args = {Query:'\'' + keyword + '\'', top: num, format:'json'};
-	extend(args, opt_args);
-	go('Image', args, function(err, response) {
-		if (err) callback(err);
-		var results = [];
-		for (var i= 0; i < response.length; i++){
-			var obj = {request:{}};
-			obj.url = response[i].SourceUrl,
-			obj.image = response[i].MediaUrl;
-			obj.request.type = 'image';
-			obj.request.keyword = keyword;
-			obj.request.ranking = i+1;
-			obj.request.platform = 'Bing';
-			obj.request.date = new Date();
-			obj.request.args = opt_args;
-			results.push(obj);
-		}
-		callback(null, results);
-	});
+    if (typeof opt_args === 'function') {
+        callback = opt_args;
+        opt_args = null;
+    }
+    var args = {Query: '\'' + keyword + '\'', top: num, format: 'json'};
+    extend(args, opt_args);
+    go('Image', args, function (err, response) {
+        if (err) callback(err);
+        var results = [];
+        for (var i = 0; i < response.length; i++) {
+            var obj = {request: {}};
+            obj.url = response[i].SourceUrl;
+            obj.image = response[i].MediaUrl;
+            obj.request.type = 'image';
+            obj.request.keyword = keyword;
+            obj.request.ranking = i + 1;
+            obj.request.platform = 'Bing';
+            obj.request.date = new Date();
+            obj.request.args = opt_args;
+            results.push(obj);
+        }
+        callback(null, results);
+    });
 }
 
-function videosSearch(keyword, num, opt_args, callback){
+function videosSearch(keyword, num, opt_args, callback) {
 // Fonction de recherche de videos via Google
 // Informations sur les arguments optionnels (opt_args): https://datamarket.azure.com/dataset/bing/search#schema
-	if (typeof opt_args === 'function'){callback = opt_args; opt_args = null;}
-	var args = {Query:'\'' + keyword + '\'', top: num, format:'json'};
-	extend(args, opt_args);
-	go('Video', args, function(err, response) {
-		if (err) callback(err);
-		var results = [];
-		for (var i= 0; i < response.length; i++){
-			var obj = {request:{}};
-			obj.url = response[i].MediaUrl,
-			obj.request.type = 'video';
-			obj.request.keyword = keyword;
-			obj.request.ranking = i+1;
-			obj.request.platform = 'Bing';
-			obj.request.date = new Date();
-			obj.request.args = opt_args;
-			results.push(obj);
-		}
-		callback(null, results);
-	});
+    if (typeof opt_args === 'function') {
+        callback = opt_args;
+        opt_args = null;
+    }
+    var args = {Query: '\'' + keyword + '\'', top: num, format: 'json'};
+    extend(args, opt_args);
+    go('Video', args, function (err, response) {
+        if (err) callback(err);
+        var results = [];
+        for (var i = 0; i < response.length; i++) {
+            var obj = {request: {}};
+            obj.url = response[i].MediaUrl;
+            obj.request.type = 'video';
+            obj.request.keyword = keyword;
+            obj.request.ranking = i + 1;
+            obj.request.platform = 'Bing';
+            obj.request.date = new Date();
+            obj.request.args = opt_args;
+            results.push(obj);
+        }
+        callback(null, results);
+    });
 }
 
 
-function go(type, args, callback){
+function go(type, args, callback) {
 // lance le nombre de requêtes search afin d'obtenir le nombre de resultat souhaités
-	var arr = listRequest(args);
-	async.concatSeries(arr, 
-			function(item, callback){
-				args.top = item.num;
-				args.skip = item.start;
-				search(type, args, callback);
-			}, 
-			function(err, response) {
-				if (err) callback(err);
-				callback(null, response);
-			}
-	);	
+    var arr = listRequest(args);
+    async.concatSeries(arr,
+        function (item, callback) {
+            args.top = item.num;
+            args.skip = item.start;
+            search(type, args, callback);
+        },
+        function (err, response) {
+            if (err) callback(err);
+            callback(null, response);
+        }
+    );
 }
 
 
-function search(type, args, callback) { 
+function search(type, args, callback) {
 // recherche en utilisant l'API Search de Bing
- 	var webSearchUrl = 'https://Basic:' + bingKey + '@api.datamarket.azure.com/Data.ashx/Bing/Search/' + type + '?';
- 	var args = querystring.stringify(args, '&$');
-	var url = webSearchUrl + args;
-	var req = request(url, function (err, response, body) {
-		if (err) {callback(err)};
-		var result = JSON.parse(body).d.results;
-  		if (!err && response.statusCode == 200) {callback(null, result);}
-	})
+    var webSearchUrl = 'https://Basic:' + bingKey + '@api.datamarket.azure.com/Data.ashx/Bing/Search/' + type + '?';
+    var args = querystring.stringify(args, '&$');
+    var url = webSearchUrl + args;
+    var req = request(url, function (err, response, body) {
+        if (err) {
+            callback(err)
+        }
+        ;
+        var result = JSON.parse(body).d.results;
+        if (!err && response.statusCode == 200) {
+            callback(null, result);
+        }
+    })
 }
 
 
-function listRequest(args){
+function listRequest(args) {
 // Permet de depasser la limitation de résultat des requêtes (10 par requête)
-		var num = args.top;
-		var nbRequest = Math.ceil(num/50);
-		var table = [];
-		for (var i=1; i <= nbRequest; i++){
-			if (i == nbRequest){
-				if (num%50 === 0) {c = 50;}
-				else {var c = num%50;}
-			}
-			else {var c = 50;}
-			table.push({num:c, start:(i-1)*50});
-			}
-		return table;
+    var num = args.top;
+    var nbRequest = Math.ceil(num / 50);
+    var table = [];
+    for (var i = 1; i <= nbRequest; i++) {
+        if (i == nbRequest) {
+            if (num % 50 === 0) {
+                c = 50;
+            }
+            else {
+                var c = num % 50;
+            }
+        }
+        else {
+            var c = 50;
+        }
+        table.push({num: c, start: (i - 1) * 50});
+    }
+    return table;
 }
 
 exports.webSearch = webSearch;
--- google.js
+++ google.js
@@ -1,8 +1,8 @@
 'use strict';
 
 /*
-auteur: tpineau 
-*/
+ auteur: tpineau
+ */
 
 var querystring = require('querystring');
 var extend = require('extend');
@@ -15,95 +15,107 @@ var customsearch = googleapis.customsearch('v1');
 var googleSearchEngine = keys.googleSearchEngine;
 var googleKey = keys.googleKey;
 
-function webSearch(keyword, num, opt_args, callback){
+function webSearch(keyword, num, opt_args, callback) {
 // Fonction de recherche de page web via Google
 // Informations sur les arguments optionnels (opt_args): https://developers.google.com/custom-search/json-api/v1/reference/cse/list?hl=FR
-	if (typeof opt_args === 'function'){callback = opt_args; opt_args = null;}
-	var args = {q: keyword, num: num, auth: googleKey, cx: googleSearchEngine};
-	extend(args, opt_args);
-	go(args, function(err, response) {
-		if (err) callback(err);
-		var results = [];
-		for (var i= 0; i < response.length; i++){
-			var obj = {request:{}};
-			obj.url = response[i].link;
-			obj.request.type = 'web';
-			obj.request.keyword = keyword;
-			obj.request.ranking = i+1;
-			obj.request.platform = 'Google';
-			obj.request.date = new Date();
-			obj.request.args = opt_args;
-			results.push(obj);
-		}
-		callback(null, results);
-	});
+    if (typeof opt_args === 'function') {
+        callback = opt_args;
+        opt_args = null;
+    }
+    var args = {q: keyword, num: num, auth: googleKey, cx: googleSearchEngine};
+    extend(args, opt_args);
+    go(args, function (err, response) {
+        if (err) callback(err);
+        var results = [];
+        for (var i = 0; i < response.length; i++) {
+            var obj = {request: {}};
+            obj.url = response[i].link;
+            obj.request.type = 'web';
+            obj.request.keyword = keyword;
+            obj.request.ranking = i + 1;
+            obj.request.platform = 'Google';
+            obj.request.date = new Date();
+            obj.request.args = opt_args;
+            results.push(obj);
+        }
+        callback(null, results);
+    });
 }
 
-function imagesSearch(keyword, num, opt_args, callback){
+function imagesSearch(keyword, num, opt_args, callback) {
 // Fonction de recherche d'images via Google
 // Informations sur les arguments optionnels (opt_args): https://developers.google.com/custom-search/json-api/v1/reference/cse/list?hl=FR
-	if (typeof opt_args === 'function'){callback = opt_args; opt_args = null;}
-	var args = {q: keyword, num: num, auth: googleKey, cx: googleSearchEngine, searchType:'image'};
-	extend(args, opt_args);
-	go(args, function(err, response) {
-		if (err) callback(err);
-		var results = [];
-		for (var i= 0; i < response.length; i++){
-			var obj = {request:{}};
-			obj.url = response[i].image.contextLink;
-			obj.image = response[i].link;
-			obj.request.type = 'image';
-			obj.request.keyword = keyword;
-			obj.request.ranking = i+1;
-			obj.request.platform = 'Google';
-			obj.request.date = new Date();
-			obj.request.args = opt_args;
-			results.push(obj);
-		}
-		callback(null, results);
-	});
+    if (typeof opt_args === 'function') {
+        callback = opt_args;
+        opt_args = null;
+    }
+    var args = {q: keyword, num: num, auth: googleKey, cx: googleSearchEngine, searchType: 'image'};
+    extend(args, opt_args);
+    go(args, function (err, response) {
+        if (err) callback(err);
+        var results = [];
+        for (var i = 0; i < response.length; i++) {
+            var obj = {request: {}};
+            obj.url = response[i].image.contextLink;
+            obj.image = response[i].link;
+            obj.request.type = 'image';
+            obj.request.keyword = keyword;
+            obj.request.ranking = i + 1;
+            obj.request.platform = 'Google';
+            obj.request.date = new Date();
+            obj.request.args = opt_args;
+            results.push(obj);
+        }
+        callback(null, results);
+    });
 }
 
-function go(args, callback){
+function go(args, callback) {
 // lance le nombre de requêtes search afin d'obtenir le nombre de resultat souhaités
-	var arr = listRequest(args);
-	async.concatSeries(arr, 
-			function(item, callback){
-				args.num = item.num;
-				args.start = item.start;
-				setTimeout(function () {
-					search(args, callback);
-				}, 1000);
-			}, 
-			function(err, response) {
-				if (err) callback(err);
-				callback(null, response);
-			}
-	);	
+    var arr = listRequest(args);
+    async.concatSeries(arr,
+        function (item, callback) {
+            args.num = item.num;
+            args.start = item.start;
+            setTimeout(function () {
+                search(args, callback);
+            }, 1000);
+        },
+        function (err, response) {
+            if (err) callback(err);
+            callback(null, response);
+        }
+    );
 }
 
-function search(args, callback){
+function search(args, callback) {
 // recherche en utilisant l'API Custom Search Engine de Google
-	customsearch.cse.list(args, function(err, data) {
-  		if (err) callback(err);
-  		callback(null, data.items);
-	});
+    customsearch.cse.list(args, function (err, data) {
+        if (err) callback(err);
+        callback(null, data.items);
+    });
 }
 
-function listRequest(args){
+function listRequest(args) {
 // Permet de depasser la limitation de résultat des requêtes (10 par requête)
-		var num = args.num;
-		var nbRequest = Math.ceil(num/10);
-		var table = [];
-		for (var i=1; i <= nbRequest; i++){
-			if (i == nbRequest){
-				if (num%10 === 0) {c = 10;}
-				else {var c = num%10;}
-			}
-			else {var c = 10;}
-			table.push({num:c, start:(i-1)*10+1});
-			}
-		return table;
+    var num = args.num;
+    var nbRequest = Math.ceil(num / 10);
+    var table = [];
+    for (var i = 1; i <= nbRequest; i++) {
+        if (i == nbRequest) {
+            if (num % 10 === 0) {
+                c = 10;
+            }
+            else {
+                var c = num % 10;
+            }
+        }
+        else {
+            var c = 10;
+        }
+        table.push({num: c, start: (i - 1) * 10 + 1});
+    }
+    return table;
 }
 
 exports.webSearch = webSearch;
