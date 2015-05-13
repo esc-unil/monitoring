1e243067ab82028695d680a7ff006fcef7ed2f34
tpineau <tpineau.esc@gmail.com>
2015-05-13 14:15:01 +0200

    add searchNew function  to google_plus storage

--- storage/google_plus.js
+++ storage/google_plus.js
@@ -16,8 +16,8 @@
  *    integrate:    indique si l'objet à déjà été parsé et intégré dans la collection d'URLs (0 si non)
  *  }
  *
- *  Les fonctions statusSearchOld / userStatusOld et statusSearchNew / userStatusNew permettent de rechercher les postes
- *  plus vieux ou plus récents (respectivement) que ceux contenus dans la DB pour un mot-clef ou un utilisateur spécifique.
+ *  Les fonctions statusSearchNew / userStatusNew permettent de rechercher les postes
+ *  plus récents que ceux contenus dans la DB pour un mot-clef ou un utilisateur spécifique.
  *
  *
  * Pour la recherche d'utilisateurs ou de pages, une recherche correspond à  un objet avec les paramètres suivants:
@@ -93,12 +93,114 @@ function userStatus(id, num, opt_args, callback){
     });
 }
 
+function statusSearchNew(keyword, num, opt_args, callback){
+// Fonction de recherche d'activités plus récente que celle de la DB pour un mot-clef particulier
+    if (typeof opt_args === 'function') {
+        callback = opt_args;
+        opt_args = {};
+    }
+    dateLastPost(keyword, function(err, lastPost) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            gplus.statusSearch(keyword, num, opt_args, function (err, response) {
+                if (err) {
+                    callback(err);
+                }
+                else {
+                    var results = [];
+                    for (var i = 0; i < response.length; i++) {
+                        if (response[i].result.published > lastPost) {
+                            response[i].integrate = 0;
+                            results.push(response[i]);
+                        }
+                    }
+                    if (results.length != 0) {
+                        mongo.insert('google_plus', results, callback);
+                    }
+                }
+            });
+        }
+    });
+}
+
+function userStatusNew(id, num, opt_args, callback){
+// Fonction de recherche d'activités plus récente que celle de la DB pour un utilisateur particulier (20 res max)
+    if (typeof opt_args === 'function') {
+        callback = opt_args;
+        opt_args = {};
+    }
+    dateLastPost(id, function(err, lastPost) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            gplus.userStatus(id, num, opt_args, function (err, response) {
+                if (err) {
+                    callback(err);
+                }
+                else {
+                    var results = [];
+                    for (var i = 0; i < response.length; i++) {
+                        if (response[i].result.published > lastPost) {
+                            response[i].integrate = 0;
+                            results.push(response[i]);
+                        }
+                    }
+                    if (results.length != 0) {
+                        mongo.insert('google_plus', results, callback);
+                    }
+                }
+            });
+        }
+    });
+}
+
+function dateLastPost(keyword, callback){
+    mongoClient.connect(mongo.mongoPath, function(err, db) {
+        if (err) {callback(err);}
+        else {
+            var collection = db.collection('google_plus');
+            collection.count(function(err, n){
+                if (n === 0){
+                    db.close();
+                    callback(null, null);
+                }
+                else{
+                    collection.count({keywords:keyword, type: 'post'}, function(err, n1){
+                        if (n1 === 0){
+                            db.close();
+                            callback(null, null);
+                        }
+                        else{
+                            collection.find({keywords: keyword, type: 'post'}).sort({'result.published': -1}).limit(1).toArray(function (err, res) {
+                                if (err) {callback(err);}
+                                else {
+                                    db.close();
+                                    callback(null, res[0].result.published);
+                                }
+                            });
+
+                        }
+                    });
+
+                }
+            });
+        }
+    });
+}
 
 
 exports.statusSearch = statusSearch;
 exports.usersSearch = usersSearch;
 exports.userStatus = userStatus;
+exports.statusSearchNew = statusSearchNew;
+exports.userStatusNew = userStatusNew;
+
 
 //statusSearch('steroid',15,function(a,b){console.log(a);});
+//statusSearchNew('steroid',200,function(a,b){console.log(a);});
 //userStatus('102090703178046743178',10,function(a,b){console.log(a);});
+//userStatusNew('117672991579759908074',20,function(a,b){console.log(a);});
 //usersSearch('steroid',10,function(a,b){console.log(a);});
