9658a0fa952dfc2b591c740f1344d8978c274182
Thomas Pineau <tpineau.esc@gmail.com>
2015-05-20 21:18:35 +0200

    tools add findAllUrl

--- integrate/tools.js
+++ integrate/tools.js
@@ -3,11 +3,56 @@
  * Created by tpineau
  */
 
-function findURL(string){
-    var reg = /https?:\/\/[^\s]+\.[^\s]+/i;
-    return reg.exec(string);
+var request = require('request');
+var async = require('async');
+
+function findAllUrls(list, callback){
+//recherche tout les URL d'une liste de textes, les URL sont non raccourci et sans doublons
+    async.concat(
+        list,
+        function(item, cbItem) {
+            cbItem(null, findURL(item));
+        },
+        function(err, res){
+            async.concat(
+                res,
+                function(obj, cbObj) {
+                    expandURL(obj, function(err, url){
+                        cbObj(null, url);
+                    });
+                },
+                function(err, results){
+                    callback(null, singleArray(results));
+                }
+            );
+        }
+    );
 }
 
-function stdURL(url){
-//(str.indexOf(substr) > -1) true si subscrib dans str
+function singleArray(array){ //supprime les doublons dans la liste
+    var result = [];
+    for (var i=0; i < array.length; i++){
+        if (result.indexOf(array[i]) === -1) {result.push(array[i]);}
+    }
+    return result;
 }
+
+function findURL(string){ //cherche tout les URL d'un texte
+    var reg = /https?:\/\/[^<>\s^!{}\[\]'"~\/\.,;]+\.[^<>\s^!{}\[\]'"~,;]+/ig;
+    return string.match(reg);
+}
+
+function expandURL(url, callback){ //recupere les URL non raccourcis
+    var reg = /https?:\/\/([^\/\s\.]+\.[^\/\s\.]+)\/[a-zA-Z0-9]+/i;
+    if (reg.exec(url) === null){callback(null, url);}
+    else {
+        request( { method: "HEAD", url: url, followAllRedirects: true },
+            function (err, res) {
+                if (err) {callback(null, url);}
+                else {callback(null, res.request.href);}
+            }
+        );
+    }
+}
+
+exports.findAllUrls = findAllUrls;
