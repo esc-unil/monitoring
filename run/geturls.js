7086d33fcdf0a39d16e907a4ff0008e137417fa9
tpineau <tpineau.esc@gmail.com>
2015-06-02 16:32:39 +0200

    test integration

--- run/geturls.js
+++ run/geturls.js
@@ -48,6 +48,4 @@ function run(database, col, target, platforms){
 
 var platforms = [youtube, reddit, google, bing, yahoo, facebook, twitter, gplus];
 
-//run(monitoring.DBrecherche, 'urls', {integrate:0}, platforms);
-
-run(monitoring.DBrecherche, 'urlst', {integrate:1}, [twitter]);
+run(monitoring.DBrecherche, 'urls', {integrate:0}, platforms);
